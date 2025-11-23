503b4cfcf782c4d5e5bbe064a465118866b7adee
bry22191919-hash <bry22191919@gmail.com>
2025-11-23 18:41:53 +0800

    Update App.jsx

--- Klassly/src/App.jsx
+++ Klassly/src/App.jsx
@@ -2,12 +2,10 @@ import { useState } from 'react'
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Login from './Pages/LogIn.jsx'
-import SignUp from './Pages/SignUp.jsx'
-<<<<<<< HEAD
+import SignUp from './Pages/SignUp.jsx
 import Dashboard from './Pages/Dashboard.jsx'
-=======
 import AssignmentForm from './Pages/AssignmentForm.jsx'
->>>>>>> ab0a908bcb8ebe2dcb3b7ede87d0c9206908a370
+
 
 function App() {
 
@@ -18,11 +16,8 @@ function App() {
       <Route path="/" element={<Login/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<SignUp/>}/>
-<<<<<<< HEAD
       <Route path="/dashboard" element={<Dashboard/>}/>
-=======
-        <Route path="/assignments/new" element={<AssignmentForm/>}/>
->>>>>>> ab0a908bcb8ebe2dcb3b7ede87d0c9206908a370
+      <Route path="/assignments/new" element={<AssignmentForm/>}/>
     </Routes>
    </Router>
   )
