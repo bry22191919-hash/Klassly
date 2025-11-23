ec67fadacc71cfc5b98bf6d6f13025f735a12d96
unknown <bry22191919@gmail.com>
2025-11-23 18:42:38 +0800

    jajja

--- Klassly/src/App.jsx
+++ Klassly/src/App.jsx
@@ -3,11 +3,8 @@ import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Login from './Pages/LogIn.jsx'
 import SignUp from './Pages/SignUp.jsx'
-<<<<<<< HEAD
 import Dashboard from './Pages/Dashboard.jsx'
-=======
 import AssignmentForm from './Pages/AssignmentForm.jsx'
->>>>>>> ab0a908bcb8ebe2dcb3b7ede87d0c9206908a370
 
 function App() {
 
@@ -18,11 +15,8 @@ function App() {
       <Route path="/" element={<Login/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<SignUp/>}/>
-<<<<<<< HEAD
       <Route path="/dashboard" element={<Dashboard/>}/>
-=======
-        <Route path="/assignments/new" element={<AssignmentForm/>}/>
->>>>>>> ab0a908bcb8ebe2dcb3b7ede87d0c9206908a370
+      <Route path="/assignments/new" element={<AssignmentForm/>}/>
     </Routes>
    </Router>
   )
